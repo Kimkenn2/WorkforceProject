82cb5623dc8429463352ffe9f26e13210778e4dc
Kenneth Kim <kkm2059@gmail.com>
2022-06-24 15:01:56 -0400

    Created Signup Component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,30 @@
 import './App.css';
+import React, {useState, useEffect} from 'react';
+import Signup from './components/Signup';
 
 function App() {
+  const [currentUser, setCurrentUser] = useState(undefined)
+  const [authenticated, setAuthenticated] = useState(false);
+
+  useEffect(() => {
+    fetch("/me", {
+      credentials: "include",
+    }).then((res) => {
+      if (res.ok) {
+        res.json().then((user) => {
+          setCurrentUser(user);
+          setAuthenticated(true);
+          console.log(user)
+        });
+      } else {
+        setAuthenticated(true);
+      }
+    });
+  }, []);
+
   return (
     <div className="App">
-     
+     <Signup />
     </div>
   );
 }
---
+++ frontend/src/components/Signup.js
@@ -0,0 +1,16 @@
+
+function Signup(){
+
+    return(
+        <div className="Signup">
+            <form>
+                <input placeholder="email"></input>
+                <input placeholder="password"></input>
+                <input placeholder="name"></input>
+                <button>Create Account</button>
+            </form>
+        </div>
+    )
+}
+
+export default Signup
