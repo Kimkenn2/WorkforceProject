b49a7f3bcc20f19f399ce0239702ee7ecbad3b35
Kenneth Kim <kkm2059@gmail.com>
2022-06-24 15:15:20 -0400

    routes and sessions

--- blog/app/controllers/users_controller.rb
+++ blog/app/controllers/users_controller.rb
@@ -1,2 +1,30 @@
 class UsersController < ApplicationController
+
+    def index
+        @users = User.all
+        render json: @users
+    end
+
+    def show
+        render json: @user
+    end
+
+    def showme
+        if current_user
+          render json: current_user, status: :ok
+        else
+          render json: "No current session stored", status: :unauthorized
+        end
+      end
+
+      
+      def create
+        user = User.create!(user_params)
+          if user.valid?
+            session[:user_id] = user.id # this is the piece that logs a user in and keeps track of users info in subsequent requests.
+            render json: user, status: :ok
+          else
+            render json: user.errors.full_messages, status: :unprocessable_entity
+          end
+      end
 end
--- blog/config/routes.rb
+++ blog/config/routes.rb
@@ -6,4 +6,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  get '/me', to: "users#showme"
+  get '/signup', to: "users#create"
 end
