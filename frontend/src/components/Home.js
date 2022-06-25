44d8417d2dc3c34ea2cca1bce35975eed97f827e
Kenneth Kim <kkm2059@gmail.com>
2022-06-25 15:22:09 -0400

    org list

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,10 +1,34 @@
-
+import Organisations from "./Organisations"
 
 function Home({currentUser, setCurrentUser, organisations}){
+    const renderOrgs = organisations.map(org =>
+        <li><Organisations org={org}/></li>
+        )
+
+    //    function noOrg() {
+    //      if(currentUser.organisation_id == false){
+    //         return(
+    //         <div>
+    //              <p>You aren't a member of any organisations. Join an existing one or create a new one.</p>
+
+    //              <h2>Organisations</h2>
+    //         <ul>
+    //             {renderOrgs}
+    //         </ul>
+    //         </div>
+    //     )}}
+    const noOrg = (
+        <div>
+             <p>You aren't a member of any organisations. Join an existing one or create a new one.</p>
+             <h2>Organisations</h2>
+            <ul className="orglist">
+                {renderOrgs}
+            </ul>
+        </div>
+    )
     return (
         <div>
-            <p>Logged in as {currentUser.name}</p>
-            {currentUser.organisation_id ? <a></a> : <p>You aren't a member of any organisations. Join an existing one or create a new one.</p>}
+            {currentUser.organisation_id ? <a></a> : noOrg}
         </div>
     )
 }
--- frontend/src/components/Organisations.js
+++ frontend/src/components/Organisations.js
@@ -1,9 +1,13 @@
 
 
-function Organisations() {
+function Organisations({org}) {
     return (
         <div>
-            
+            <span>{org.name}   </span>
+            <span>Edit </span>
+            <span>Join</span>
         </div>
     )
 }
+
+export default Organisations
